56c9b8f2ef2c0d00e4eb501d451d37d3f7b81b2a
Emiliano Maccaferri <donglerio@unimore.it>
2025-02-08 18:34:01 +0100

    add: tunneling ok, find a way to reuse the same connection

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
-use std::{collections::HashMap, sync::Arc};
+use std::{
+    collections::HashMap,
+    convert::Infallible,
+    future::Future,
+    io::{self, Error},
+    net::{AddrParseError, IpAddr, Ipv4Addr},
+    sync::Arc,
+};
 
 use russh::{
     client::{self, Handler},
     keys::{load_secret_key, PrivateKeyWithHashAlg},
     Channel, ChannelMsg, ChannelStream, CryptoVec,
 };
+use thiserror::Error;
 use tokio::{
     io::{stdin, stdout, AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
+    net::{
+        tcp::{OwnedReadHalf, OwnedWriteHalf},
+        TcpListener, TcpStream,
+    },
     select,
-    sync::{mpsc, Mutex},
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        Mutex, RwLock,
+    },
+    task::JoinHandle,
 };
 
 #[tokio::main]
 async fn main() {
-    let key = load_secret_key("/home/macca/.ssh/macca-desktop", None).unwrap();
+    let (tx, mut rx) = mpsc::channel(32);
+    let key = load_secret_key("/Users/macca/.ssh/macca-macbook", None).unwrap();
     let config = client::Config::default();
     let config = Arc::new(config);
-    let mut session = client::connect(config, ("116.203.141.67", 22), ClientHandler {})
+    let mut session = client::connect(config, ("116.203.141.67", 22), ClientHandler::new(tx))
         .await
         .unwrap();
     let auth_res = session
@@ -33,41 +49,106 @@ async fn main() {
 
     dbg!(&auth_res);
     session.tcpip_forward("0.0.0.0", 9000).await.unwrap(); // this asks the server to open the port 9000 on its side
-    let mut channel = session.channel_open_session().await.unwrap();
-    let code = loop {
-        let Some(msg) = channel.wait().await else {
-            panic!("noooo");
-        };
-        match msg {
-            ChannelMsg::Data { ref data } => {
-                tokio::io::stdout().write_all(data).await.unwrap();
-                tokio::io::stdout().flush().await.unwrap();
-            }
-            ChannelMsg::ExtendedData { ref data, ext: 1 } => {
-                tokio::io::stderr().write_all(data).await.unwrap();
-                tokio::io::stderr().flush().await.unwrap();
-            }
-            ChannelMsg::Success => (),
-            ChannelMsg::Close => break 0,
-            ChannelMsg::ExitStatus { exit_status } => {
-                channel.eof().await.unwrap();
-                break exit_status;
-            }
-            msg => panic!("muzunnu"),
-        }
-    };
+    session.channel_open_session().await.unwrap();
+    while let Some((mut tunnel, channel)) = rx.recv().await {
+        tokio::spawn(async move {
+            println!(
+                "new tunnel running: {}:{}",
+                tunnel.remote_addr, tunnel.remote_port
+            );
+            tunnel.run(channel).await.unwrap();
+        });
+    }
 }
 
-struct ClientHandler {}
-
+struct ClientHandler {
+    tx: Sender<(Tunnel, Channel<client::Msg>)>,
+}
 struct Tunnel {
-    remote_addr: String,
+    remote_addr: IpAddr,
     remote_port: u16,
-    channel: Channel<client::Msg>,
+    rx: Arc<Mutex<OwnedReadHalf>>,
+    tx: Arc<Mutex<OwnedWriteHalf>>,
+}
+#[derive(Error, Debug)]
+enum TunnelError {
+    #[error("invalid address supplied: {0}")]
+    InvalidAddress(String),
+    #[error("io error: {1}")]
+    Io(io::Error, String),
+}
+impl ClientHandler {
+    pub fn new(tx: Sender<(Tunnel, Channel<client::Msg>)>) -> Self {
+        ClientHandler { tx }
+    }
+}
+impl From<AddrParseError> for TunnelError {
+    fn from(value: AddrParseError) -> Self {
+        Self::InvalidAddress(value.to_string())
+    }
+}
+impl From<Error> for TunnelError {
+    fn from(value: Error) -> Self {
+        let str = value.to_string();
+        Self::Io(value, str)
+    }
 }
 
 impl Tunnel {
-    pub async fn run(&self) {}
+    pub async fn connect(remote_addr: &str, remote_port: u16) -> Result<Tunnel, TunnelError> {
+        let remote_addr = remote_addr.parse()?;
+        let conn = TcpStream::connect((remote_addr, remote_port)).await?;
+        let (rx, tx) = conn.into_split();
+        let rx = Arc::new(Mutex::new(rx));
+        let tx = Arc::new(Mutex::new(tx));
+        Ok(Tunnel {
+            remote_addr,
+            remote_port,
+            rx,
+            tx,
+        })
+    }
+    pub fn run(&mut self, channel: Channel<client::Msg>) -> JoinHandle<()> {
+        let mut writer = channel.make_writer();
+        let mut stream = channel.into_stream();
+        let tx = self.tx.clone();
+        let rx = self.rx.clone();
+
+        let reading_handle = tokio::spawn(async move {
+            loop {
+                let mut buf = vec![0u8; 4096];
+                if let Ok(n) = stream.read(&mut buf).await {
+                    if n == 0 {
+                        break;
+                    }
+                    dbg!(&n);
+                    tx.lock().await.write_all(&buf[..n]).await.unwrap();
+                }
+            }
+        });
+        let writing_handle = tokio::spawn(async move {
+            loop {
+                let mut buf = vec![0u8; 4096];
+                if let Ok(n) = rx.lock().await.read(&mut buf).await {
+                    if n == 0 {
+                        break;
+                    }
+                    dbg!(&n);
+                    writer.write_all(&buf[..n]).await.unwrap();
+                }
+            }
+        });
+        tokio::spawn(async move {
+            select! {
+                _w = writing_handle => {
+                    println!("service disconnected");
+                },
+                _r = reading_handle => {
+                    println!("client disconnected");
+                }
+            }
+        })
+    }
 }
 
 impl Handler for ClientHandler {
@@ -83,128 +164,16 @@ impl Handler for ClientHandler {
     }
     async fn server_channel_open_forwarded_tcpip(
         &mut self,
-        mut channel: Channel<client::Msg>,
-        connected_address: &str,
-        connected_port: u32,
-        originator_address: &str,
-        originator_port: u32,
-        session: &mut client::Session,
+        channel: Channel<client::Msg>,
+        _connected_address: &str,
+        _connected_port: u32,
+        _originator_address: &str,
+        _originator_port: u32,
+        _session: &mut client::Session,
     ) -> Result<(), Self::Error> {
-        let sshid = String::from_utf8_lossy(session.remote_sshid());
-        dbg!(
-            sshid,
-            connected_address,
-            connected_port,
-            originator_address,
-            originator_port
-        );
-        let remote = TcpStream::connect("localhost:8080").await.unwrap();
-        let (mut rx, mut tx) = remote.into_split();
+        let mut tunnel = Tunnel::connect("127.0.0.1", 8080).await.unwrap();
+        self.tx.send((tunnel, channel)).await.unwrap();
 
-        let mut writer = channel.make_writer();
-        let mut stream = channel.into_stream();
-
-        tokio::spawn(async move {
-            loop {
-                let mut buf = vec![0u8; 4096];
-                if let Ok(n) = stream.read(&mut buf).await {
-                    if n == 0 {
-                        break;
-                    }
-                    dbg!(&n);
-                    tx.write_all(&buf[..n]).await.unwrap();
-                }
-            }
-        });
-        tokio::spawn(async move {
-            loop {
-                let mut buf = vec![0u8; 4096];
-                if let Ok(n) = rx.read(&mut buf).await {
-                    if n == 0 {
-                        break;
-                    }
-                    dbg!(&n);
-                    writer.write_all(&buf[..n]).await.unwrap();
-                }
-            }
-        });
         Ok(())
     }
 }
-
-/*struct TunnelManager {
-    tunnels: HashMap<String, Tunnel>,
-}
-struct Tunnel {
-    stream: ChannelStream<client::Msg>,
-}
-
-impl Tunnel {
-    pub async fn start(&self) {
-        let remote_conn = TcpStream::connect("localhost:8080").await.unwrap();
-        let (mut rx, mut tx) = remote_conn.into_split();
-
-        tokio::spawn(async move {
-            let mut buf = vec![0u8; 4096];
-            while let Ok(n) = rx.read(&mut buf).await {
-                if n == 0 {
-                    println!("stream ended");
-                    break;
-                }
-                self.stream.write_all(&buf[..n]).await.unwrap();
-            }
-        });
-    }
-}
-
-impl TunnelManager {
-    pub async fn add_tunnel(&mut self, name: &str, channel: Channel<client::Msg>) -> &Tunnel {
-        self.tunnels.insert(
-            name.into(),
-            Tunnel {
-                stream: channel.into_stream(),
-            },
-        );
-        self.tunnels.get(name.into()).unwrap()
-    }
-
-    pub async fn start_tunnel(&self, name: &str) {
-
-        if let Some(tunnel) = self.tunnels.get(name) {
-            let t = tunnel.clone();
-            println!("starting: {}", name);
-            let remote_listener = TcpStream::connect("localhost:8080").await.unwrap();
-            let (mut rx, mut tx) = remote_listener.into_split();
-            let writer = Arc::new(Mutex::new(t.channel.make_writer()));
-
-            let get_reader = reader.clone();
-
-            tokio::spawn(async move {
-                let mut buf = vec![0u8; 4096];
-                loop {
-                    let mut reader = get_reader.lock().await;
-                    if let Ok(n) = reader.read(&mut buf).await {
-                        if n == 0 {
-                            println!("stream ended");
-                            break;
-                        }
-                        tx.write_all(&buf[..n]).await.unwrap();
-                    }
-                }
-            });
-
-            tokio::spawn(async move {
-                let mut buf = vec![0u8; 4096];
-                while let Ok(n) = rx.read(&mut buf).await {
-                    /*if n == 0 {
-                        println!("stream ended");
-                        break;
-                    }
-                    let mut stream = tunnel_stream_send.lock().await;
-                    stream.write_all(&buf[..n]).await.unwrap();*/
-                }
-            });
-        }
-    }
-}
-*/
